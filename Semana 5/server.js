09569caf9b12c1fdb9e3964c20c4c957077f051a
Carolina Valenzuela <carocaro8936@gmail.com>
2026-09-07 23:00:22 -0300

    Agrego server actualizado Semana 5

--- Semana 5/server.js
+++ Semana 5/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const { ApolloServer, gql } = require('apollo-server-express');
+
 const Usuario = require('./models/usuario');
+const Producto = require('./models/producto');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/bdunab2')
+
+mongoose.connect('mongodb://127.0.0.1:27017/dulcehogar')
     .then(() => {
         console.log('MongoDB conectado');
     })
@@ -15,42 +18,91 @@ mongoose.connect('mongodb://127.0.0.1:27017/bdunab2')
         console.log(error);
     });
 
+
 const typeDefs = gql`
+
     type Usuario {
         id: ID!
         nombre: String!
         pass: String!
+        rol: String!
     }
 
     input UsuarioInput {
         nombre: String!
         pass: String!
+        rol: String!
+    }
+
+    type Producto {
+        id: ID!
+        nombre: String!
+        categoria: String!
+        precio: Float!
+        stock: Int!
+    }
+
+    input ProductoInput {
+        nombre: String!
+        categoria: String!
+        precio: Float!
+        stock: Int!
     }
 
     type Alert {
         message: String
     }
 
+
     type Query {
+
         getUsuarios: [Usuario]
-        getUsuariosById(id: ID!): Usuario
+        getUsuarioById(id: ID!): Usuario
+
+        getProductos: [Producto]
+        getProductoById(id: ID!): Producto
     }
 
+
     type Mutation {
+
+
         addUsuario(input: UsuarioInput): Usuario
-        updateUsuario(id: ID!, input: UsuarioInput): Usuario
+
+        updateUsuario(
+            id: ID!,
+            input: UsuarioInput
+        ): Usuario
+
         delUsuario(id: ID!): Alert
+
+
+
+        addProducto(input: ProductoInput): Producto
+
+        updateProducto(
+            id: ID!,
+            input: ProductoInput
+        ): Producto
+
+        delProducto(id: ID!): Alert
     }
 `;
 
+
 const resolvers = {
+
     Query: {
+
         async getUsuarios() {
+
             const usuarios = await Usuario.find();
+
             return usuarios;
         },
 
-        async getUsuariosById(obj, { id }) {
+        async getUsuarioById(obj, { id }) {
+
             const usuario = await Usuario.findById(id);
 
             if (usuario == null) {
@@ -58,11 +110,34 @@ const resolvers = {
             }
 
             return usuario;
+        },
+
+
+        async getProductos() {
+
+            const productos = await Producto.find();
+
+            return productos;
+        },
+
+
+        async getProductoById(obj, { id }) {
+
+            const producto = await Producto.findById(id);
+
+            if (producto == null) {
+                return null;
+            }
+
+            return producto;
         }
     },
 
+
     Mutation: {
+
         async addUsuario(obj, { input }) {
+
             const usuario = new Usuario(input);
 
             await usuario.save();
@@ -71,26 +146,66 @@ const resolvers = {
         },
 
         async updateUsuario(obj, { id, input }) {
+
             const usuario = await Usuario.findByIdAndUpdate(
                 id,
                 input,
-                { new: true }
+                {
+                    new: true
+                }
             );
 
             return usuario;
         },
 
+
         async delUsuario(obj, { id }) {
+
             await Usuario.findByIdAndDelete(id);
 
             return {
                 message: 'Usuario eliminado'
             };
+        },
+
+
+        async addProducto(obj, { input }) {
+
+            const producto = new Producto(input);
+
+            await producto.save();
+
+            return producto;
+        },
+
+
+        async updateProducto(obj, { id, input }) {
+
+            const producto = await Producto.findByIdAndUpdate(
+                id,
+                input,
+                {
+                    new: true
+                }
+            );
+
+            return producto;
+        },
+
+        async delProducto(obj, { id }) {
+
+            await Producto.findByIdAndDelete(id);
+
+            return {
+                message: 'Producto eliminado'
+            };
         }
     }
 };
 
+
 async function startServer() {
+
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers
@@ -108,8 +223,10 @@ async function startServer() {
     });
 
     app.listen(8090, () => {
+
         console.log('GraphQL iniciado');
         console.log('Servidor: http://localhost:8090/graphql');
+
     });
 }
 
