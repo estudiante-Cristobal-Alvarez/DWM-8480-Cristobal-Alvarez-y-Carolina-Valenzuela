f31ee1d0e4a64249fa45bcef88a9ccc0c86b26f1
Cristobal Alvarez <cristobal.alvarezcataldo@gmail.com>
2026-09-07 22:26:12 -0300

    Se agrega producto

---
+++ Semana 5/models/producto.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productoSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Producto', productoSchema);
